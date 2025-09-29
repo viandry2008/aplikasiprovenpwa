e24f9587261ab626f0c212885d9d07e32d542f22
viandry8002 <viandry8002@gmail.com>
2025-09-29 11:22:17 +0700

    update icon

---
+++ app.config.js
@@ -0,0 +1,23 @@
+export default {
+    expo: {
+        name: "SHRM RFID",
+        slug: "shrm-rfid",
+        version: "1.0.0",
+        orientation: "portrait",
+        platforms: ["ios", "android", "web"],
+        icon: "./assets/Logo.png",
+        splash: {
+            image: "./assets/Logo.png",
+            resizeMode: "contain",
+            backgroundColor: "#ffffff"
+        },
+        web: {
+            favicon: "./icons/icon-192.png",
+            bundler: "metro",
+            name: "SHRM RFID",
+            shortName: "SHRM RFID",
+            themeColor: "#000000",
+            backgroundColor: "#ffffff"
+        }
+    }
+};
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api/";
+// export const API_BASE_URL = "http://127.0.0.1:8000/api/"; // Localhost
+export const API_BASE_URL = "https://test.smarthrm-provenforceindonesia.site/api/"; // testing server
 
 export const ENDPOINTS = {
     LOGIN: "login",
